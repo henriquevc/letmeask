e1e2bd4ff0e902f978d9e2178372c1c6c5091319
henriquevc <henriquevc93@gmail.com>
2021-06-22 14:32:47 -0300

    first commit and config firebase

---
+++ src/services/firebase.ts
@@ -0,0 +1,11 @@
+import firebase from 'firebase/app'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCLrtvxFKVqeRPS_yvfTDZJb19qkt8PYc8",
+    authDomain: "letmeask-79d48.firebaseapp.com",
+    databaseURL: "https://letmeask-79d48-default-rtdb.firebaseio.com",
+    projectId: "letmeask-79d48",
+    storageBucket: "letmeask-79d48.appspot.com",
+    messagingSenderId: "1067965497595",
+    appId: "1:1067965497595:web:df817a57e6aff4a9bdf05e"
+  };
