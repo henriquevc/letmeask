fde8e934ce437269e0ca56cce2651882fa79b245
Henrique <henriquevc93@gmail.com>
2021-06-24 14:27:16 -0300

    setup firebase and definition env variables

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,11 +1,18 @@
 import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCLrtvxFKVqeRPS_yvfTDZJb19qkt8PYc8",
-    authDomain: "letmeask-79d48.firebaseapp.com",
-    databaseURL: "https://letmeask-79d48-default-rtdb.firebaseio.com",
-    projectId: "letmeask-79d48",
-    storageBucket: "letmeask-79d48.appspot.com",
-    messagingSenderId: "1067965497595",
-    appId: "1:1067965497595:web:df817a57e6aff4a9bdf05e"
-  };
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN ,
+    databaseURL: process.env.REACT_APP_DATABASE_URL ,
+    projectId: process.env.REACT_APP_PROJECT_ID ,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET ,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID ,
+    appId: process.env.REACT_APP_APP_ID 
+}
+
+firebase.initializeApp(firebaseConfig)
+
+export const auth = firebase.auth()
+export const database = firebase.database()
