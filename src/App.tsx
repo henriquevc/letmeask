053c60f3ef42becdb9bd8872b5ce93b1e6b8c601
Henrique <henriquevc93@gmail.com>
2021-06-26 15:03:18 -0300

    implementing routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import { NewRoom } from "./pages/NewRoom";
-
+import { BrowserRouter, Route } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { NewRoom } from './pages/NewRoom';
 import './styles/global.scss'
 
 function App() {
   return (
     <div>
-      <NewRoom />
+      <BrowserRouter>
+        <Route path="/" exact component={Home}></Route>
+        <Route path="/rooms/new" component={NewRoom}></Route>
+      </BrowserRouter>
     </div>
 
   );
